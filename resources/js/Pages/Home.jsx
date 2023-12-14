5ead0491e4ec171577848b8fc7c326d0c0e9c5f4
chubi-x <chubixax@protonmail.com>
2023-12-14 18:38:12 +0000

    new groups modal

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -4,31 +4,30 @@ import Todos from "@/Pages/Todos/Todos.jsx";
 import Todo from "@/Pages/Todos/Todo.jsx";
 import Groups from "@/Pages/Groups/Groups.jsx";
 import Tabs from "@/Components/Tabs.jsx";
-import Modal from "@/Components/Modal.jsx";
-
-
-
-export default function Home({todos, groups}) {
+import GroupModal from "@/Components/GroupModal.jsx";
+import Authenticated from "@/Layouts/AuthenticatedLayout.jsx";
+export default function Home({auth, todos, groups}) {
     const [openTab, setOpenTab] = React.useState(1);
     const todosTab = () => {
         return <Todos todos={todos}>
                 {todos.map((todo) => (
                   !todo.recycled &&  <Todo key={todo.id} todo={todo}>
-                        <Modal id={`todo-${todo.id}-modal`} groups={groups} title="Add to Group" todo={todo}/>
+                        <GroupModal id={`todo-${todo.id}-modal`} groups={groups} title="Add to Group" todo={todo}/>
                     </Todo>
                 ))}
             </Todos>
     }
     return (
-        <>
-            <div className="w-2/3 mx-auto w-full flex flex-col p-20 space-y-6 items-center justify-center">
-                <Head title="Todo App" />
-                <h1 className="text-4xl font-bold">Todo App</h1>
-                <Tabs color="rose" openTab={openTab} setOpenTab={setOpenTab} />
-                <div className="shadow-md shadow-red-500 h-auto w-full py-10 px-32 md:px-16 rounded-md ">
-                    {openTab === 1 ? todosTab(): <Groups groups={groups}/>}
+            <Authenticated user={auth.user}>
+                <div className="w-2/3 mx-auto w-full flex flex-col p-20 space-y-6 items-center justify-center">
+                    <Head title="Todo App" />
+                    <h1 className="text-4xl font-bold">Todo App</h1>
+                    <Tabs color="rose" openTab={openTab} setOpenTab={setOpenTab} />
+                    <div className="shadow-md shadow-red-500 h-auto w-full py-10 px-32 md:px-16 rounded-md ">
+                        {openTab === 1 ? todosTab(): <Groups groups={groups}/>}
+                    </div>
                 </div>
-            </div>
-            </>
+            </Authenticated>
+
     );
 }
