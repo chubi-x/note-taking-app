e32d46baa55200c25b3d75dd6e4fc03175600fda
chubi-x <chubixax@protonmail.com>
2023-12-14 16:32:49 +0000

    refactor: misc re

--- resources/js/Pages/Groups/Groups.jsx
+++ resources/js/Pages/Groups/Groups.jsx
@@ -17,13 +17,11 @@ export default function Groups({groups}) {
     return <>
         <h2 className="text-xl font-bold mb-4">Groups</h2>
         <ul className="space-y-2">
-
             {groups && groups.map((group) => (
                 <Group key={group.id} group={group} className="border-b-2 border-gray-400"/>
             ))}
-
             <li>
-                <form onSubmit={(e)=>handleSubmit(e)} className="flex flex-col">
+                <form onSubmit={(e)=>handleSubmit(e)} className="flex mt-10 border-b-2 border-gray-400 flex-col">
                     <input className="bg-white rounded-lg w-full focus:border-blue-300 focus:ring-2 border-none font-medium cursor-pointer p-4"
                            type="text" placeholder="New Group" onChange={(e) => setGroup('name', e.target.value)}
                     />
